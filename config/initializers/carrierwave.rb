db0b69bf336b3b1ad2e16156131df6c3ecf641bc
mattun3 <60844878+mattun3@users.noreply.github.com>
2020-06-28 16:55:30 +0900

    carrierwave.rb 修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,18 +5,18 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_provider = 'fog/aws'
-  # config.fog_credentials = {
-  #   provider: 'AWS',
-  #   aws_access_key_id: Rails.application.credentials[:aws][:access_key_id],
-  #   aws_secret_access_key: Rails.application.credentials[:aws][:secret_access_key],
-  #   region: 'ap-northeast-1'
-  # }
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-    aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+    aws_access_key_id: Rails.application.credentials[:aws][:access_key_id],
+    aws_secret_access_key: Rails.application.credentials[:aws][:secret_access_key],
     region: 'ap-northeast-1'
   }
+  # config.fog_credentials = {
+  #   provider: 'AWS',
+  #   aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+  #   aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+  #   region: 'ap-northeast-1'
+  # }
 
   config.fog_directory  = 'freemarket-sample68e'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket-sample68e'
